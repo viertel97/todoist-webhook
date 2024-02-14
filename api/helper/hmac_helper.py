161b0c89e2d9cbbf2eccf3c1c8bdd84a7092e8f2
viertel97 <janik.viertel@outlook.com>
2024-02-14 17:33:25 +0100

    added hmac verification

---
+++ api/helper/hmac_helper.py
@@ -0,0 +1,14 @@
+import base64
+import hashlib
+import hmac
+
+from quarter_lib.akeyless import get_secrets
+from starlette.requests import Request
+
+webhook_verification_token = get_secrets("todoist/webhook_verification_token")
+
+
+async def verify_hmac(request: Request):
+    calculated_hmac = hmac.new(webhook_verification_token.encode("utf-8"), await request.body(), hashlib.sha256).digest()
+    calculated_hmac_base64 = base64.b64encode(calculated_hmac).decode()
+    return request.headers.get("X-Todoist-Hmac-SHA256") == calculated_hmac_base64
--- api/router/main_router.py
+++ api/router/main_router.py
@@ -3,6 +3,7 @@ from fastapi.encoders import jsonable_encoder
 from quarter_lib.logging import setup_logging
 from starlette.requests import Request
 
+from api.helper.hmac_helper import verify_hmac
 from shared.models.webhook import Webhook
 from main_worker import process_webhook
 from quarter_lib.akeyless import get_secrets
@@ -12,17 +13,12 @@ import hashlib
 logger = setup_logging(__name__)
 router = APIRouter()
 
-verification_token, webhook_client_secret = get_secrets(["todoist/webhook_verification_token", "todoist/webhook_client_secret"])
-
 
 @router.post("/")
 async def persist_webhook(request: Request, webhook: Webhook):
-    hmac_to_check = request.headers["X-Todoist-Hmac-SHA256"]
-    signature = hmac.new(
-        key=webhook_client_secret,
-        msg=str(webhook.dict()),
-        digestmod=hashlib.sha256
-    ).hexdigest().upper()
+    is_valid = await verify_hmac(request)
+    if not is_valid:
+        return {"status": "invalid hmac"}
     logger.info(f"Received Todoist webhook: {webhook.event_name}")
     result = process_webhook.delay(jsonable_encoder(webhook))
     result_output = result.get()
