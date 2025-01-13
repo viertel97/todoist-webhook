20a949cd740213e0dbc9add926f3950210bcdc63
Janik Viertel <janik.viertel@outlook.com>
2025-01-13 11:15:39 +0100

    added transaction

--- worker/services/database_service.py
+++ worker/services/database_service.py
@@ -16,6 +16,13 @@ def insert_webhook_into_database(webhook: Webhook):
 	for column in ["event_data~labels", "event_data~item~labels"]:
 		if column in df.columns:
 			df[column] = df[column].apply(lambda x: str(x) if isinstance(x, list) and len(x) > 0 else math.nan)
-	result = df.to_sql("todoist_webhooks", con=ENGINE, if_exists="append", index=False)
-	logger.info(f"Inserted {result} webhook(s) into database")
+	with ENGINE.connect() as connection:
+		try:
+			result = df.to_sql("todoist_webhooks", con=connection, if_exists="append", index=False, chunksize=50)
+			connection.commit()
+			logger.info(f"Inserted {result} webhook(s) into database")
+		except Exception as e:
+			connection.rollback()
+			logger.error(f"Failed to insert webhook into database: {e}")
+			raise e
 	return result
