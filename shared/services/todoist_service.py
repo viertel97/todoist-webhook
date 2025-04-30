1b81cb980e99afa781484285ff5c59e4d65483f8
Janik Viertel <janik.viertel@outlook.com>
2025-04-30 10:26:11 +0200

    added preservation of old tasks

--- shared/services/todoist_service.py
+++ shared/services/todoist_service.py
@@ -16,7 +16,10 @@ def add_comment_to_thread(task_id: str, comment: str):
 
 def add_label_to_task(task_id: str, label: str):
 	logger.info(f"Adding label '{label}' to task {task_id}")
-	result = TODOIST_API.update_task(task_id=task_id, labels=[label])
+	task = TODOIST_API.get_task(task_id=task_id)
+	labels = task.labels
+	labels.append(label)
+	result = TODOIST_API.update_task(task_id=task_id, labels=list(set(labels)))
 	return result
 
 def get_comments_by_task_id(task_id: str):
